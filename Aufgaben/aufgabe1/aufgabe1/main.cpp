a3fc38d7758d5c1a80325111da7ce7cb11593129
André <45215556+andrealbers@users.noreply.github.com>
2019-10-19 16:40:44 +0200

    Update main.cpp

--- Aufgaben/aufgabe1/aufgabe1/main.cpp
+++ Aufgaben/aufgabe1/aufgabe1/main.cpp
@@ -7,12 +7,12 @@ void aufgabe1(void) {
 	/*
 	Welche Indizes sind erlaubt?
 		Zugriff nur auf initialisierte Felder erlaubt | Bei Initialisierung: i > 0 | Sonst: i >=0
-	Was passiert, wenn Sie beim Zugriff auf das Array einen zu groﬂen oder zu kleinen Index verwenden (z.B. -1 oder 21)?
+	Was passiert, wenn Sie beim Zugriff auf das Array einen zu gro√üen oder zu kleinen Index verwenden (z.B. -1 oder 21)?
 		Programm versucht auf einen nicht allozierten Speicherbereich zuzugreifen. Es wird irgendein Wert ausgegeben.
-	Gibt es Mˆglichkeiten, zur Laufzeit die Grˆﬂe des Arrays zu ver‰ndern?
+	Gibt es M√∂glichkeiten, zur Laufzeit die Gr√∂√üe des Arrays zu ver√§ndern?
 		int neugroesse;
 		cin >> neugroesse;
-		int *zahlen = malloc(neugroesse*sizeof(int));  //Abfangen bei neugroesse
+		int *zahlen = malloc(neugroesse*sizeof(int));  //Abfangen bei neugroesse==NULL
 	*/
 	int zahlen[10];
 
@@ -26,7 +26,7 @@ void aufgabe1(void) {
 
 void aufgabe2(void) {
 	/*
-	Welche Vorteile bieten new/delete gegen¸ber malloc()/free() und was sind die Unterschiede?
+	Welche Vorteile bieten new/delete gegen√ºber malloc()/free() und was sind die Unterschiede?
 
 	*/
 	double* ptrZahlen = new double[10];
@@ -44,11 +44,11 @@ void aufgabe2(void) {
 
 void aufgabe3(void) {
 	/*
-	Erkl‰ren Sie, was die folgenden Methoden bewirken: size(), at(), push_back().
-		size(): Gibt Anzahl der beinhalteten Elemente an | at(): vector.at(3) -> R¸ckgabe vom Wert von vector[3] | push_back(): Element wird am Ende des Vectors eingef¸gt
-	Welche Vorteile besitzt die Methode at() gegen¸ber dem Zugriffoperator []?
-		Bei ‹berschreitung des Wertebereichs des Vectors->Fehlermeldung. Nur Zugriff auf existierende Elemente des Vectors mˆglich.
-	Ist es bei einem vector mˆglich, die Grˆﬂe w‰hrend der Laufzeit zu ver‰ndern? Wenn ja, wie?
+	Erkl√§ren Sie, was die folgenden Methoden bewirken: size(), at(), push_back().
+		size(): Gibt Anzahl der beinhalteten Elemente an | at(): vector.at(3) -> R√ºckgabe vom Wert von vector[3] | push_back(): Element wird am Ende des Vectors eingef√ºgt
+	Welche Vorteile besitzt die Methode at() gegen√ºber dem Zugriffoperator []?
+		Bei √úberschreitung des Wertebereichs des Vectors->Fehlermeldung. Nur Zugriff auf existierende Elemente des Vectors m√∂glich.
+	Ist es bei einem vector m√∂glich, die Gr√∂√üe w√§hrend der Laufzeit zu ver√§ndern? Wenn ja, wie?
 		vector.resize(5); oder vector.shrink_to_fit();
 	*/
 	vector<double> vectorzahlen(0);
